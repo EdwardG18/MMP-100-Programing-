f5504a334ef478a04ddedf832d2d3d9a2a986790
EdwardG18 <91147859+EdwardG18@users.noreply.github.com>
2021-11-23 12:50:40 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+//The setup function only happens once
+function setup() {
+	createCanvas(2550, 2550); //create a 500px X 500px canvas
+}
+
+//The draw function happens over and over again
+function draw() {
+  background(0,204,102); //an RGB color for the canvas' background
+  //circle
+  stroke(455,455,233) // an RGB color for the circle's border
+  fill(255,255,127,150); // an RGB color for the inside of the circle (the last number refers to transparency (min. 0, max. 255))
+  ellipse(width/2,height/2,333,400); // center of canvas, 20px dia
+}
+//The draw function happens over and over again
+function draw() {
+  background(0,204,102); //an RGB color for the canvas' background
+  //circle
+  stroke(455,455,233) // an RGB color for the circle's border
+  fill(255,255,127,150); // an RGB color for the inside of the circle (the last number refers to transparency (min. 0, max. 255))
+  ellipse(width/2,height/5,340,500); // center of canvas, 20px dia
+}
+//The draw function happens over and over again
+function draw() {
+  background(0,204,102); //an RGB color for the canvas' background
+  //circle
+  stroke(455,455,233) // an RGB color for the circle's border
+  fill(255,255,127,150); // an RGB color for the inside of the circle (the last number refers to transparency (min. 0, max. 255))
+  ellipse(width/2,height/4,353,600); // center of canvas, 20px dia
+}
